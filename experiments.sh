52be4677c2381b098e17c91f5a75d9fd858d8361
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-04-26 15:19:35 +0200

    random experiment

--- experiments.sh
+++ experiments.sh
@@ -1,23 +1,15 @@
 #!/bin/bash -x
 
 iterations=15
-models="ibm1 ibm2 jump"
+models="jump"
 
 for model in $models; do
     python main.py --iterations=$iterations --model=$model --sampling_method=uniform
 done
 
-for model in $models; do
-    python main.py --iterations=$iterations --model=$model --sampling_method=uniform --lower
-done
-
 for seed in "1 2 3"; do
-    for model in "ibm2 jump"; do
+    for model in "jump"; do
         python main.py --iterations=$iterations --model=$model --sampling_method=random --seed=$seed
     done
 done
 
-# # hard to automate selection of 'best' ibm1 model to start from, so doing this manually instead...
-# for model in $models; do
-#     python main.py --iterations=$iterations --model=$model --sampling_method=uniform --probabilities=ibm1-uniform.pkl
-# done
